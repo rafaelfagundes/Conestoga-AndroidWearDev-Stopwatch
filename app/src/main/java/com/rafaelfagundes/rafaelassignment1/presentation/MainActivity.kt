0ca15ca1a2fb55cf964738dcc03c829471a146e6
Rafael Fagundes <rafaelcflima@gmail.com>
2024-02-03 15:05:12 -0500

    Remove unused import

--- app/src/main/java/com/rafaelfagundes/rafaelassignment1/presentation/MainActivity.kt
+++ app/src/main/java/com/rafaelfagundes/rafaelassignment1/presentation/MainActivity.kt
@@ -20,7 +20,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
